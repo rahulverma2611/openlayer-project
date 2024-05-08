222a6091e925ba6b1b79acfee80e9fac77d1a68f
rahul <rv22090@gmail.com>
2024-05-08 23:24:57 +0530

    draw functionality commit

--- my-app/app/components/DistanceMeasure.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DistanceMeasure: React.FC = () => {
-  return <div></div>;
-};
-
-export default DistanceMeasure;
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="">
       {/* <MapComponent /> */}
 
-      {/* <DrawOnMap /> */}
+      <DrawOnMap />
     </main>
   );
 }
